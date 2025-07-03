7c8667667333363c6c889a5a714a816fca2f0c3f
kartik-jabitsoft <kartiksharma@jabitsoft.com>
2025-07-03 17:41:42 +0530

    apply filter

--- components/Products/CategotyProduct.js
+++ components/Products/CategotyProduct.js
@@ -1,18 +1,14 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import styles from "./Products.module.scss";
-import { Container, Row, Col } from "react-bootstrap";
-import Loader from "./../shared/Loader/Loader";
+import { Container, Row } from "react-bootstrap";
 import { useRouter } from "next/router";
 import ProductItem from "../shared/Products/ProductItem";
 import PaginationUi from "../shared/Pagination/PaginationUi";
-import { Button, Collapse } from "antd";
-import { Checkbox } from "antd";
-import Image from "next/image";
+import { Button, Spin, message } from "antd";
+import { Checkbox, Collapse } from "antd";
 import axios from "axios";
 import { endPoint, envUrl } from "../../utils/factory";
-import { Checklist, Delete } from "@mui/icons-material";
 import NoProduct from "./NoProduct";
-import { useMediaQuery } from "@material-ui/core";
 
 const CategotyProduct = ({
   products,
@@ -22,202 +18,158 @@ const CategotyProduct = ({
   handlePagination,
   total,
 }) => {
-  const defaultPageSize = 18;
   const router = useRouter();
-  const urlPath = router.query.index;
-  const subUrlPath = router.query.indextwo;
-  const subsubUrlPath = router.query.indexthree;
+  const { index: urlPath, indextwo: subUrlPath } = router.query;
   const paths = router.asPath.replace("/", "").replace(/-/g, " ");
-  // const filterParams = router;
-  console.log("!!!!!!00000000", router);
-  // console.log("!!!!!!11111111111", urlPath);
-  // console.log("!!!!!!!!!!2222222222", subUrlPath);
-  // console.log("!!!!!!!!!!33333", subsubUrlPath);
-  // console.log("!!!!!!!!!!44444", paths);
-  // console.log("!!!!!!!!!!5555", router.asPath);
-
+  
   const [filterData, setFilterData] = useState([]);
-  console.log("ffffffff", filterData);
-  const ProductGrid = () =>
-    products &&
-    products.map((product, i) => {
-      return (
-        // eslint-disable-next-line react/jsx-key
-        <ProductItem product={product} />
-      );
-    });
-  // ?Filters=|custmFilter1=50mm+20mm|custmFilter2=20mm+30mm|custmFilter=5+1
-  // const throttle = (func, delay = 1000) => {
-  //   let timerId;
-  //   let lastExecTime = 0;
-  //   return (...args) => {
-  //     const currentTime = new Date().getTime();
-  //     if (currentTime - lastExecTime > delay) {
-  //       func.apply(null, args);
-  //       lastExecTime = currentTime;
-  //     } else {
-  //       clearTimeout(timerId);
-  //       timerId = setTimeout(() => {
-  //         func.apply(null, args);
-  //         lastExecTime = new Date().getTime();
-  //       }, delay);
-  //     }
-  //   };
-  // };
-  function handleCheck(e) {
-    console.log(e.target.name + "=" + e.target.value);
-    console.log(`checked = ${e.target.checked},${e.target.value}`);
-    if (e.target.checked) {
-      if (router.query.hasOwnProperty(e.target.name)) {
-        router.replace({
-          query: {
-            ...router.query,
-            [e.target.name]: router.query[`${e.target.name}`].concat(
-              "+",
-              e.target.value.toString()
-            ),
-            page: 1,
-          },
-        });
-        // router.push({
-        //   pathname: `/${urlPath}/${subUrlPath}`,
-        //   query : {"custmFilter" + e.target.name = e.target.value},
-        // });
-      } else {
-        router.replace({
-          query: {
-            ...router.query,
-            [e.target.name]: e.target.value.toString(),
-            page: 1,
-          },
-        });
-      }
-    } else {
-      if (router.query.hasOwnProperty(e.target.name)) {
-        let currentFilterValues = router.query[`${e.target.name}`];
-        // console.log();
-        let arr = currentFilterValues.split("+");
-        let newFilterValues = arr
-          .filter((a) => {
-            return a !== e.target.value;
-          })
-          .join("+");
-        console.log("!!!! newFilterValues", newFilterValues);
-        if (newFilterValues.length == 0) {
-          // router.replace(router.query[`${e.target.name}`], undefined, {
-          //   shallow: true,
-          // });
-          delete router.query[`${e.target.name}`];
-          router.replace({
-            query: {
-              ...router.query,
-            },
-          });
-        } else {
-          router.replace({
-            query: {
-              ...router.query,
-              [e.target.name]: newFilterValues,
-            },
-          });
-        }
-      }
-    }
-  }
+  const [selectedFilters, setSelectedFilters] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [initialLoad, setInitialLoad] = useState(true);
 
+  // Load initial filter data
   useEffect(() => {
-    if (Object.keys(router.query).length > 2) {
-      router.replace(`/${urlPath}/${subUrlPath}`);
-      console.log("<<<<<<<<", router);
-    }
-    axios
-      .post(
-        `${envUrl.baseUrl}${endPoint.getProductFilters}`,
-        { paths: paths },
-        {
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then(function (response) {
-        console.log("ffff22222 response", response);
-        if (response.status == 200) {
-          setFilterData(response.data.data);
-        } else if (response.status == 204) {
-          setFilterData([]);
-        }
-      })
-      .catch((error) => {
-        console.log("API error in getProductFilters :::::", error);
-        // setLoader(false);
-      })
-      .finally(() => {});
-  }, []);
+    const fetchFilters = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.post(
+          `${envUrl.baseUrl}${endPoint.getProductFilters}`,
+          { paths },
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-  const items = filterData.map((f, index) => {
-    return {
+        if (response.status === 200) {
+          setFilterData(response.data.data || []);
+        }
+      } catch (error) {
+        console.error("API error in getProductFilters:", error);
+        message.error("Failed to load filters");
+      } finally {
+        setLoading(false);
+        setInitialLoad(false);
+      }
+    };
+
+    if (initialLoad) {
+      fetchFilters();
+    }
+  }, [paths, initialLoad]);
+
+  // Handle checkbox changes
+  const handleCheckboxChange = (filterName, value, checked) => {
+    setSelectedFilters(prev => {
+      const newFilters = { ...prev };
+      const filterKey = `custmFilter${filterName}`;
+      
+      // If the checkbox is checked
+      if (checked) {
+        // Add the value to the filter's array
+        newFilters[filterKey] = newFilters[filterKey] 
+          ? [...newFilters[filterKey].split('+'), value].join('+') 
+          : value;
+      } else {
+        // Remove the value from the filter's array
+        const values = newFilters[filterKey]?.split('+') || [];
+        const newValues = values.filter(v => v !== value);
+        
+        if (newValues.length > 0) {
+          newFilters[filterKey] = newValues.join('+');
+        } else {
+          delete newFilters[filterKey]; // Remove the filter if no values are left
+        }
+      }
+      return newFilters;
+    });
+  };
+
+  // Apply selected filters
+  const applyFilters = () => {
+    router.replace({
+      pathname: `/${urlPath}/${subUrlPath}`,
+      query: {
+        ...router.query,
+        ...selectedFilters,
+        page: 1, // Reset to first page when applying new filters
+      },
+    });
+  };
+
+  // Reset all filters
+  const handleReset = () => {
+    setSelectedFilters({});
+    router.replace(`/${urlPath}/${subUrlPath}`);
+  };
+
+  // Generate filter UI items
+  const filterItems = useMemo(() => 
+    filterData.map((f, index) => ({
       key: index,
       label: f.filterName,
-      children: f.filter_values.map((fv, i) => {
-        if (fv) {
-          return (
-            <div className="checkbox" key={i}>
-              <Checkbox
-                key={i}
-                value={fv}
-                onChange={handleCheck}
-                name={`custmFilter${f.custmFilterNumber}`}
-              >
-                {fv}
-              </Checkbox>
-            </div>
-          );
-        } else {
-          return null;
-        }
-      }),
-    };
-  });
-
-  const onChange = (key) => {
-    console.log(key);
-  };
-
-  const handleReset = () => {
-    router.replace(`/${urlPath}/${subUrlPath}`);
-    router.reload();
-  };
+      children: f.filter_values.map((fv, i) => 
+        fv ? (
+          <div className="checkbox" key={i}>
+            <Checkbox
+              value={fv}
+              // Check if the value is included in the selected filters
+              checked={selectedFilters[`custmFilter${f.custmFilterNumber}`]?.split('+').includes(fv)}
+              onChange={(e) => handleCheckboxChange(
+                f.custmFilterNumber, 
+                fv, 
+                e.target.checked
+              )}
+            >
+              {fv}
+            </Checkbox>
+          </div>
+        ) : null
+      ),
+    })),
+    [filterData, selectedFilters]
+  );
 
   return (
-    <>
+    <Spin spinning={loading}>
       {products && products.length > 0 ? (
-        <>
-          <div className={styles.main_container}>
-            <div className={styles.filter_container}>
-              <div className={styles.filter_header}>
-                <span>Filters</span>
-                <div>
-                  <img src={"/Group.png"} width={20} height={20} />
-                </div>
-              </div>
-              <div>
-                <Collapse
-                  expandIconPosition="right"
-                  items={items}
-                  // defaultActiveKey={["1"]}
-                  onChange={onChange}
-                />
-              </div>
-              <div className={styles.clear_button_container}>
-                <Button onClick={handleReset}>Clear</Button>
-              </div>
+        <div className={styles.main_container}>
+          <div className={styles.filter_container}>
+            <div className={styles.filter_header}>
+              <span>Filters</span>
             </div>
+            
+            <Collapse
+              expandIconPosition="right"
+              items={filterItems}
+              defaultActiveKey={filterData.map((_, i) => i.toString())}
+            />
 
-            <div className={styles.ProductGrid}>
-              <ProductGrid />
+            <div className={styles.clear_button_container}>
+            <Button 
+                type="text" 
+                onClick={applyFilters}
+                disabled={loading || Object.keys(selectedFilters).length === 0}
+                style={{width:"5rem"}}
+              >
+                Apply
+              </Button>
+
+              <Button 
+                type="default"
+                onClick={handleReset}
+                disabled={loading || Object.keys(selectedFilters).length === 0}
+                style={{width:"5rem"}}
+              >
+                Clear
+              </Button>
             </div>
+          </div>
+
+          <div className={styles.ProductGrid}>
+            {products.map((product, i) => <ProductItem key={i} product={product} />)}
           </div>
 
           <div className={styles.paginationUI}>
@@ -228,7 +180,7 @@ const CategotyProduct = ({
               pageSize={pageSize}
             />
           </div>
-        </>
+        </div>
       ) : (
         <Container fluid>
           <Row className="justify-content-md-center">
@@ -236,7 +188,7 @@ const CategotyProduct = ({
           </Row>
         </Container>
       )}
-    </>
+    </Spin>
   );
 };
 
